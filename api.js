3c7a6870c27fcf68304680db4b2c3bed690378bd
Aitor <91470257+Aitormon@users.noreply.github.com>
2024-05-03 12:27:27 +0200

    Add files via upload

--- api.js
+++ api.js
@@ -79,63 +79,15 @@ async function muestranoticias(){
        titulo = document.createElement("a");
        titulo.style.color="white";
        titulo.href=noticia.url;
-       titulo.textContent = noticia.url;
+       titulo.target="_blank";
+
+      
+       titulo.textContent = noticia.url ;
        Listanoticias.appendChild(titulo);
     
-       /*  ListaEventos.appendChild(document.createElement("br")); */
-
-/*         let title3 = document.createElement("h3");
-        title3.innerText="Fecha de inicio del evento";
-        title3.classList.add('titulo2');
-        ListaEventos.appendChild(title3); */
-
-        /* ListaEventos.appendChild(document.createElement("br")); */
-
-/*         let li = document.createElement("li");
-        li.innerText = evento.startTime.replace("T"," Hora: ");
-        li.innerText = li.innerText.replace("Z","");
-        li.style.fontSize = "30px";
-        li.style.fontStyle = "bold";
-        
-        ListaEventos.appendChild(li); */
-
-        /* ListaEventos.appendChild(document.createElement("br")); */
-
-
-/*         let title2 = document.createElement("h3");
-        title2.innerText="Fecha de Fin del evento";
-        title2.classList.add('titulo2');
-        ListaEventos.appendChild(title2);
-        
-
-        li = document.createElement("li");
-        li.innerText = evento.endTime.replace("T"," Hora: ");
-        li.innerText = li.innerText.replace("Z","");
-        li.style.fontSize = "30px";
-        li.style.fontStyle = "bold";
-        ListaEventos.appendChild(li); */
+       
 
         Listanoticias.appendChild(document.createElement("br"));
-
-
-
-///BOTON
-/*         let boton = document.createElement('button');
-    boton.style.class = "animated-button";
-    boton.textContent = 'VOLVER';
-    boton.id = 'volver';
-    boton.style.backgroundColor = 'red';
-    boton.style.color = 'white';
-
-
-    boton.addEventListener('click', function() {
-        window.location.href = 'eventos.html';
-    });
-
-        ListaEventos.appendChild(boton); */
-
-///BOTON
-
 
 
 
